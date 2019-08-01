19771bd2171b10facda78deec50032c4d0656475
Hiro Asari <asari.ruby@gmail.com>
2019-08-01 11:59:20 -0400

    Handle multiple timers
    By passing an optional suffix to `travis_time_*` functions, we track
    multiple timers which can potentially overlap.

--- lib/travis/build/bash/travis_time_finish.bash
+++ lib/travis/build/bash/travis_time_finish.bash
@@ -3,7 +3,13 @@ travis_time_finish() {
   local travis_timer_end_time
   travis_timer_end_time="$(travis_nanoseconds)"
   local duration
+  local timer_id
+  if [[ $# -gt 0 ]]; then
+    timer_id="TRAVIS_TIMER_ID_$1"
+  else
+    timer_id=${TRAVIS_TIMER_ID}
+  fi
   duration="$((travis_timer_end_time - TRAVIS_TIMER_START_TIME))"
-  echo -en "travis_time:end:${TRAVIS_TIMER_ID}:start=${TRAVIS_TIMER_START_TIME},finish=${travis_timer_end_time},duration=${duration}\\r${ANSI_CLEAR}"
+  echo -en "travis_time:end:${timer_id}:start=${TRAVIS_TIMER_START_TIME},finish=${travis_timer_end_time},duration=${duration}\\r${ANSI_CLEAR}"
   return "${result}"
 }
--- lib/travis/build/bash/travis_time_start.bash
+++ lib/travis/build/bash/travis_time_start.bash
@@ -1,6 +1,13 @@
 travis_time_start() {
-  TRAVIS_TIMER_ID="$(printf %08x $((RANDOM * RANDOM)))"
+  local timer_id="$(printf %08x $((RANDOM * RANDOM)))"
+  if [[ $# -gt 0 ]]; then
+    eval "TRAVIS_TIMER_ID_$1=${timer_id}"
+    export TRAVIS_TIMER_ID_$1
+  else
+    TRAVIS_TIMER_ID="$(printf %08x $((RANDOM * RANDOM)))"
+    export TRAVIS_TIMER_ID
+  fi
   TRAVIS_TIMER_START_TIME="$(travis_nanoseconds)"
-  export TRAVIS_TIMER_ID TRAVIS_TIMER_START_TIME
+  export TRAVIS_TIMER_START_TIME
   echo -en "travis_time:start:$TRAVIS_TIMER_ID\\r${ANSI_CLEAR}"
 }
